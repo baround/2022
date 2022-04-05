4b3788dd6c709696c1c6e109905fc81e4360487c
luciacenetiempo <luciacenetiempo@gmail.com>
2022-04-05 10:53:50 +0200

    edit domain in share

--- components/component/component-share.js
+++ components/component/component-share.js
@@ -18,7 +18,7 @@ export const Whatsapp = (props) => (
 )
 export default function Share({ color }){
   const router = useRouter();
-  const thisUrl = `https://2022-baround.vercel.app${router.asPath}`;
+  const thisUrl = `https://baround.it${router.asPath}`;
   return (
     <section className={`${styles.share} share share--${color}`}>
       <div className={`${styles.share__wrapper}`}>
