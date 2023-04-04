13484cfb41a9edf77baa0f28ae9674170c8614d8
luciacen <lucia.cenetiempo@luxottica.com>
2023-04-04 17:13:00 +0200

    baround 2023
    baround 2023

---
+++ pages/diario/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Header from '../../components/header'
+import Page from '../../components/page'
+import Article from '../../components/homepage/article'
+import Footer from '../../components/footer'
+import { getSortedPostsData } from '../../lib/query'
+import styles from '../fullpage.module.scss'
+const categoryType = 'posts';
+const categorySlug = 'diario'; 
+const postTitle = 'Diario';
+const pageID = '970'; 
+export default function Post({ page, posts }) {
+  let postList = [];
+  posts.forEach((x, y, z) => !(y % 4) ? postList.push(z.slice(y, y + 4)) : '');
+  return (
+    <div className='baround'>
+      <Head>
+        <title>Baround - {postTitle}</title>
+      </Head>
+      <Header />
+      <Page pageData={page} pageType={categorySlug} />
+      <section id={categorySlug} className={`${styles.fullpage} ${categorySlug}`}>
+        {postList.map((block, index) => {
+          return (
+            <div className={`${styles.fullpage__row} ${categorySlug}__row`} key={index}>
+              <div className={`${styles.fullpage__row__wrap} ${categorySlug}__row__wrap`}>
+                {
+                  block.map((post, index) => {
+                    return (
+                      <Article block={post} classList={`${styles.fullpage__post} ${categorySlug}__post post post__card`} baseLink={`/${categorySlug}`} key={index} pageType={categorySlug}/>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          )
+        })}
+      </section>
+      <Footer />
+    </div>
+  )
+}
+export async function getStaticProps() {
+  const page = await getSortedPostsData('pages/'+pageID)
+  const posts = await getSortedPostsData(categoryType + '?page=1&per_page=100')
+  return {
+    props: {
+      page,
+      posts
+    }
+  }
+}
---
+++ pages/locali/[id].js
@@ -0,0 +1,143 @@
+import { getSortedPostsData, getAllPostIds, getPostData } from '../../lib/query'
+import {useEffect, useRef, useState} from 'react';
+
+import Head from 'next/head'
+import Header from '../../components/header'
+import Page from '../../components/page'
+import Article from '../../components/homepage/article'
+import Share from '../../components/component/component-share'
+import Map from '../../components/component/component-map'
+import Footer from '../../components/footer'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+const postType = 'locali';
+const postTitle = 'Locale';
+
+export default function Post({ postData, allPosts, itinerari }) {
+  let postList = [...allPosts];
+  let thisPost = postData;
+  let postIndex = postList.findIndex(post => post.id == thisPost.id);
+  postList.splice(postIndex, 1);
+  let latestPosts = [...postList].slice(0, 6);
+  let nearItinerari =  [];
+  postData.acf.itinerari_vicini.map((itinerario, index) => {
+    nearItinerari.push(itinerario.ID);
+  })
+  let itinerariVicini = itinerari.filter(item => nearItinerari.includes(item.id));
+  let nearBar =  [];
+  postData.acf.locali_simili.map((bar, index) => {
+    nearBar.push(bar.ID);
+  })
+  let barVicini = allPosts.filter(item => nearBar.includes(item.id));
+  const [mapObject, setMapObject] = useState(null);
+
+  return (
+    <div className='baround'>
+      <Head>
+        <title>Baround - {postTitle} - {postData.yoast_title}</title>
+      </Head>
+      <Header />
+      <Page pageData={postData} pageType={postType} />
+
+      <Map setMapObject={setMapObject} postData={postData} pageType={'locale'} />
+
+      <Share color='blue'/>
+      <section className='morePost morePost--locali contentText'>
+        <div className='morePost__wrap'>
+          <div className='morePost__wrap__row'>
+            <h3>Bar simili a questo</h3>
+          </div>
+          <Swiper
+            modules={[Pagination]}
+            spaceBetween={24}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 3,
+              },
+              920: {
+                slidesPerView: 4,
+              },
+            }}
+          >
+            {barVicini.map((block, index) => {
+              return (
+
+                <SwiperSlide key={index}>
+                  <Article block={block} classList={'post post__card'} baseLink={`/${postType}`} pageType={postType} />
+                </SwiperSlide>
+              )
+            })}
+
+          </Swiper>
+        </div>
+      </section>
+      <section className='morePost morePost--itinerari contentText'>
+        <div className='morePost__wrap'>
+          <div className='morePost__wrap__row'>
+            <h3>Itinerari nelle vicinanze</h3>
+          </div>
+          <Swiper
+            modules={[Pagination]}
+            spaceBetween={24}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 3,
+              },
+              920: {
+                slidesPerView: 4,
+              },
+            }}
+          >
+            {itinerariVicini.map((block, index) => {
+              return (
+
+                <SwiperSlide key={index}>
+                  <Article block={block} classList={'post post__card'} baseLink={`/itinerari`} pageType={'itinerari'} />
+                </SwiperSlide>
+              )
+            })}
+
+          </Swiper>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  )
+}
+
+
+
+
+export async function getStaticPaths() {
+  const paths = await getAllPostIds(postType + '?page=1&per_page=100')
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(postType + '?slug=' + params.id)
+  const posts = await getSortedPostsData(postType + '?page=1&per_page=100')
+  const itinerari = await getSortedPostsData('itinerari?page=1&per_page=100')
+  return {
+    props: {
+      postData: postData[0],
+      allPosts: posts,
+      itinerari: itinerari
+    }
+  }
+}   
